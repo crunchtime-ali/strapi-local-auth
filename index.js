feb64957e58d891898dec2109d9e9ae51128e35d
Hedgehog <hedgehog@ali.dj>
2016-07-20 10:39:36 +0200

    replaced faux login with real one based on Strapi models

--- index.js
+++ index.js
@@ -28,22 +28,43 @@ module.exports = function (strapi) {
 
 				var user = {id: 1, username: 'test'};
 
-				passport.serializeUser(function (user, done) {
-					done(null, user.id);
+				passport.serializeUser(function(user, done) {
+					strapi.log.info("serialize User called", user);
+					done(null, user.id)
 				});
 
-				passport.deserializeUser(function (id, done) {
-					done(null, user);
+
+				passport.deserializeUser(function(id, done) {
+					console.log(id)
+					strapi.log.info("deserializeUser called");
+					strapi.services.userlogin.fetch({id: id})
+					.then(function(user) {
+						if (user !== false) {
+							done(null, {id: user.attributes.id , username: user.attributes.username});
+						} else {
+							done(null, false, {message: "Couldn't retrieve user"});
+						}
+					})
+					.catch(function(err) {
+						strapi.log.info("Deserialize error");
+						done(null, false, { message: err});
+					});
 				});
 
 				var LocalStrategy = passportLocal.Strategy;
-				passport.use(new LocalStrategy(function (username, password, done) {
-					// retrieve user ...
-					if (username === 'test' && password === 'test') {
-						done(null, user);
-					} else {
+				passport.use(new LocalStrategy((username, password, done) => {
+					strapi.log.info("executing local strategy");
+
+					// Retrieve user
+					strapi.services.userlogin.getUser(username, password)
+					.then(function(user) {
+						// console.log("resolved to", user);
+						done(null, {id: user.attributes.id , username: user.attributes.username, role: user.attributes.role});
+					})
+					.catch(function(err) {
+						strapi.log.info("Login Error", err);
 						done(null, false);
-					}
+					});
 				}));
 
 				// Make this passport instance globally available
@@ -55,41 +76,3 @@ module.exports = function (strapi) {
 	};
 	return hook;
 };
-
-			/* passport.serializeUser(function(user, done) {
-				strapi.log.info("serialize User called", user);
-			  done(null, user.id)
-			})
-
-			passport.deserializeUser(function(id, done) {
-				console.log(id)
-				strapi.log.info("deserializeUser called");
-				strapi.services.userlogin.fetch({id: id})
-				.then(function(user) {
-					if (user !== false) {
-						done(null, {id: user.attributes.id , username: user.attributes.username});
-					} else {
-						done(null, false, {message: "Couldn't retrieve user"});
-					}
-				})
-				.catch(function(err) {
-					strapi.log.info("Deserialize error");
-					done(null, false, { message: err});
-				});
-			});
-
-			var LocalStrategy = passportLocal.Strategy;
-			passport.use(new LocalStrategy((username, password, done) => {
-				strapi.log.info("executing local strategy");
-
-				// Retrieve user
-				strapi.services.userlogin.getUser(username, password)
-				.then(function(user) {
-					// console.log("resolved to", user);
-					done(null, {id: user.attributes.id , username: user.attributes.username, role: user.attributes.role});
-				})
-				.catch(function(err) {
-					strapi.log.info("Login Error", err);
-					done(null, false);
-				});
-			}));*/
