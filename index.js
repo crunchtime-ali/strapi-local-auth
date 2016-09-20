089321c97c903c6beb51edc29e42b2843074ed0d
Hedgehog <hedgehog@ali.dj>
2016-09-20 13:16:07 +0200

    using E-Mail instead of username

--- index.js
+++ index.js
@@ -1,75 +1,73 @@
-'use strict';
+'use strict'
 
-const passport = require('koa-passport');
-const passportLocal = require('passport-local');
+const passport = require('koa-passport')
+const passportLocal = require('passport-local')
 /**
  * Strapi passport hook
  */
 
 module.exports = function (strapi) {
-	const hook = {
-		/**
-		 * Default options
-		 */
-		defaults: {
-		},
+  const hook = {
+    /**
+     * Default options
+     */
+    defaults: {
+    },
     /**
      * Initialize the hook
      */
-		initialize: function (cb) {
-			console.log('strapi-auth init called');
-			strapi.after(["hook:generic-session:loaded"], () => {
+    initialize: function (cb) {
+      //console.log('strapi-auth init called')
+      strapi.after(['hook:generic-session:loaded'], () => {
+        strapi.log.info('strapi-local-auth initialized')
 
-				console.log('strapi-local-auth initialized');
+        strapi.app.use(passport.initialize())
+        strapi.app.use(passport.session())
 
+        passport.serializeUser(function (user, done) {
+          strapi.log.info('serialize User called', user)
+          done(null, user.id)
+        })
 
-				strapi.app.use(passport.initialize());
-				strapi.app.use(passport.session());
+        passport.deserializeUser(function (id, done) {
+          strapi.log.info('deserializeUser called with ID', id)
+          strapi.services.jsonapi.fetch(strapi.models.user, {id: id}, { include:[] })
+            .then(function (user) {
+              if (user !== false && user !== null) {
+                done(null, {id: user.attributes.id})
+              } else {
+                done(null, false, {message: "Couldn't retrieve user"})
+              }
+            })
+            .catch(function (err) {
+              strapi.log.info('Deserialize error')
+              strapi.log.info(err)
+              done(null, false, { message: err })
+            })
+        })
 
-				var user = {id: 1, username: 'Fire'};
+        var LocalStrategy = passportLocal.Strategy
+        passport.use(new LocalStrategy({
+          usernameField: 'email'
+        },
+        (username, password, done) => {
+          strapi.log.info('executing local strategy')
 
-				passport.serializeUser(function(user, done) {
-					strapi.log.info("serialize User called", user);
-					done(null, user.id)
-				});
+          // Retrieve user
+          console.log(username, password)
+          strapi.services.userlogin.getUser(username, password)
+            .then(function (user) {
+              done(null, {id: user.relations.user.id, email: user.attributes.email, role: user.attributes.role})
+            })
+            .catch(function (err) {
+              strapi.log.info('Login Error', err)
+              done(null, false)
+            })
+        }))
 
-
-				passport.deserializeUser(function(id, done) {
-					strapi.log.info("deserializeUser called");
-					strapi.services.jsonapi.fetch(strapi.models.userlogin, {id: id})
-					.then(function(user) {
-						if (user !== false) {
-							done(null, {id: user.attributes.id , username: user.attributes.username});
-						} else {
-							done(null, false, {message: "Couldn't retrieve user"});
-						}
-					})
-					.catch(function(err) {
-						strapi.log.info("Deserialize error");
-						done(null, false, { message: err});
-					});
-				});
-
-				var LocalStrategy = passportLocal.Strategy;
-				passport.use(new LocalStrategy((username, password, done) => {
-
-					strapi.log.info("executing local strategy");
-
-					// Retrieve user
-					strapi.services.userlogin.getUser(username, password)
-					.then(function(user) {
-						done(null, {id: user.relations.user.id , username: user.attributes.username, role: user.attributes.role});
-					})
-					.catch(function(err) {
-						strapi.log.info("Login Error", err);
-						done(null, false);
-					});
-				}));
-
-				console.log('local auth strategy chosen');
-				cb();
-			});
-		}
-	};
-	return hook;
-};
+        cb()
+      })
+    }
+  }
+  return hook
+}
